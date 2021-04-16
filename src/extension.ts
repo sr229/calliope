9aeff9bdbdc34097072d3f386a3e2537673ab78d
Ayane Satomi <chinodesuuu@gmail.com>
2021-04-16 23:27:03 +0800

    create basic layout for view
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,64 @@
 /// <reference path='../vscode.proposed.d.ts'/>
 import * as vscode from 'vscode';
 
-class MothershipWorkspaces extends vscode.TreeItem {
-	//TODO: find out what kind of data we want to instantiate here!
+class MothershipHost extends vscode.TreeItem {
+	hostName?: string;
+
+	constructor(hostname: string, hostAddress: string) {
+		super(`${hostname}`);
+		this.hostName = hostname;
+	}
 }
 
+class MothershipHosts extends vscode.TreeItem {
+	readonly hosts = new Map<number, MothershipHost>();
+
+	constructor() {
+		super('Hosts', vscode.TreeItemCollapsibleState.Expanded);
+	}
+}
+
+type MothershipHostElement = MothershipHosts | MothershipHost;
+
+class MothershipHostsTreeDataProvider implements vscode.TreeDataProvider<MothershipHostElement> {
+	readonly hosts = new MothershipHosts();
+
+	protected readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<MothershipHostElement | undefined>();
+	readonly onDidChangeTreeData = this.onDidChangeTreeDataEmitter.event;
+
+	constructor() {}
+
+	getTreeItem(element: MothershipHostElement): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: MothershipHostElement): vscode.ProviderResult<MothershipHostElement[]> {
+		if (!element) {
+			return [this.hosts];
+		}
+
+		if (element === this.hosts) {
+			return [...this.hosts.hosts.values()];
+		}
+
+		return [];
+	}
+
+	getParent(element: MothershipHostElement): MothershipHostElement | undefined {
+		if (element instanceof MothershipHost) {
+			return this.hosts;
+		}
+
+		return undefined;
+	}
+}
 
 export function activate(context: vscode.ExtensionContext) {
+	const mothershipHostsTreeDataProvider = new MothershipHostsTreeDataProvider();
+
+	const workspaceView = vscode.window.createTreeView('mothershipHosts', {
+		treeDataProvider: mothershipHostsTreeDataProvider
+	});
 	console.log("Project Mothership has loaded successfully!");
 }
 
